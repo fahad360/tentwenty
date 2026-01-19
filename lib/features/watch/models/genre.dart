c7ff424174b4754afa2db63d7bf6ffaae2c8a35c
Fahad <m.fahadfayyaz1@gmail.com>
2026-01-20 01:26:26 +0500

    Implement search functionality and genre display in WatchScreen

---
+++ lib/features/watch/models/genre.dart
@@ -0,0 +1,6 @@
+class Genre {
+  final String title;
+  final String imageUrl;
+
+  Genre({required this.title, required this.imageUrl});
+}
---
+++ lib/features/watch/widgets/genre_card.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import '../models/genre.dart';
+
+class GenreCard extends StatelessWidget {
+  final Genre genre;
+
+  const GenreCard({super.key, required this.genre});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        image: DecorationImage(
+          image: NetworkImage(genre.imageUrl),
+          fit: BoxFit.cover,
+          colorFilter: ColorFilter.mode(
+            Colors.black.withOpacity(0.2),
+            BlendMode.darken,
+          ),
+        ),
+      ),
+      alignment: Alignment.bottomLeft,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(
+          genre.title,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+}
